223943d7e8613147adb7e9d87ecd4589726320a1
jlrestad <31015266+jlrestad@users.noreply.github.com>
2018-02-18 10:07:49 -0800

    Update SunDistance.java

--- SunDistance.java
+++ SunDistance.java
@@ -2,52 +2,53 @@
  * Program Name:		SunDistance.java
  * Date Created:		February 16th, 2018
  * Author(s): 			Jes Restad
- * Title: 				HW02
+ * Title: 			HW02
  * Description: 		Lists constants for the planet distances from the sun.
- * Last Updated: 		February 16th, 2018 
+ * Last Updated: 		February 17th, 2018 
  * ********************************************************************************* */
 package space;
 
 //planet distances from the sun
 public class SunDistance {  
-	public double MERCURY = 5.79*Math.pow(10, 10);
-	public double VENUS = 1.08*Math.pow(10, 11);
-	public double EARTH = 1.50*Math.pow(10, 11);
-	public double MARS = 2.28*Math.pow(10, 11);
-	public double JUPITER = 7.78*Math.pow(10, 11);
-	public double SATURN = 1.43*Math.pow(10, 12);
-	public double URANUS = 2.87*Math.pow(10, 12);
-	public double NEPTUNE = 4.50*Math.pow(10,12);
+	private final double MERCURY = 5.79*Math.pow(10, 10);
+	private final double VENUS = 1.08*Math.pow(10, 11);
+	private final double EARTH = 1.50*Math.pow(10, 11);
+	private final double MARS = 2.28*Math.pow(10, 11);
+	private final double JUPITER = 7.78*Math.pow(10, 11);
+	private final double SATURN = 1.43*Math.pow(10, 12);
+	private final double URANUS = 2.87*Math.pow(10, 12);
+	private final double NEPTUNE = 4.50*Math.pow(10,12);
 	
-	public double getDistanceMercury() {
+	
+	public double mercury() {
 		return MERCURY;
 	}
 	
-	public double getDistanceVenus() {
+	public double venus() {
 		return VENUS;
 	}
 	
-	public double getDistanceEarth() {
+	public double earth() {
 		return EARTH;
 	}
 	
-	public double getDistanceMars() {
+	public double mars() {
 		return MARS;
 	}
 	
-	public double getDistanceJupiter() {
+	public double jupiter() {
 		return JUPITER;
 	}
 	
-	public double getDistanceSaturn() {
+	public double saturn() {
 		return SATURN;
 	}
 	
-	public double getDistanceUranus() {
+	public double uranus() {
 		return URANUS;
 	}
 	
-	public double getDistanceNeptune() {
+	public double neptune() {
 		return NEPTUNE;
 	}
 		
