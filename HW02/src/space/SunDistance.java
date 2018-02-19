780184ec08a9d3e368567da3cdce238498d79d70
jlrestad <31015266+jlrestad@users.noreply.github.com>
2018-02-18 17:01:44 -0800

    Update SunDistance.java

--- HW02/src/space/SunDistance.java
+++ HW02/src/space/SunDistance.java
@@ -10,14 +10,14 @@ package space;
 
 //planet distances from the sun
 public class SunDistance {  
-	public double MERCURY = 5.79*Math.pow(10, 10);
-	public double VENUS = 1.08*Math.pow(10, 11);
-	public double EARTH = 1.50*Math.pow(10, 11);
-	public double MARS = 2.28*Math.pow(10, 11);
-	public double JUPITER = 7.78*Math.pow(10, 11);
-	public double SATURN = 1.43*Math.pow(10, 12);
-	public double URANUS = 2.87*Math.pow(10, 12);
-	public double NEPTUNE = 4.50*Math.pow(10,12);
+	public final double MERCURY = 5.79*Math.pow(10, 10);
+	public final double VENUS = 1.08*Math.pow(10, 11);
+	public final double EARTH = 1.50*Math.pow(10, 11);
+	public final double MARS = 2.28*Math.pow(10, 11);
+	public final double JUPITER = 7.78*Math.pow(10, 11);
+	public final double SATURN = 1.43*Math.pow(10, 12);
+	public final double URANUS = 2.87*Math.pow(10, 12);
+	public final double NEPTUNE = 4.50*Math.pow(10,12);
 	
 	public double getDistanceMercury() {
 		return MERCURY;
