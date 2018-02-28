b8b7a14ece20f1f05fd52e1e3bed8c3f97bb0fbe
jlrestad <31015266+jlrestad@users.noreply.github.com>
2018-02-27 21:35:48 -0800

    Added static

--- HW02/src/space/Mass.java
+++ HW02/src/space/Mass.java
@@ -2,7 +2,7 @@
  * Program Name:		Mass.java
  * Date Created:		February 17th, 2018
  * Author(s): 			Marianne Goldin
- * Title: 				HW02
+ * Title: 			HW02
  * Description: 		Lists constants for planet masses in kg.
  * Last Updated: 		February 18th, 2018 
  * ********************************************************************************* */
@@ -11,47 +11,12 @@ package space;
 
 //planet masses in kg
 public class Mass {
-	public final double MERCURY = 3.18 * Math.pow(10,23);
-	public final double VENUS = 4.88 * Math.pow(10,24);
-	public final double EARTH = 5.98 * Math.pow(10,24);
-	public final double MARS = 6.42 * Math.pow(10,23);
-	public final double JUPITER = 1.90 * Math.pow(10,27);
-	public final double SATURN = 5.68 * Math.pow(10,25);
-	public final double URANUS = 8.68 * Math.pow(10,25);
-	public final double NEPTUNE = 1.03 * Math.pow(10,26);
-	
-		
-	public double getMassMercury() {
-		return MERCURY;
-	}
-	
-	public double getMassVenus() {
-		return VENUS;
-	}
-	
-	public double getMassEarth() {
-		return EARTH;
-	}
-	
-	public double getMassMars() {
-		return MARS;
-	}
-	
-	public double getMassJupiter() {
-		return JUPITER;
-	}
-	
-	public double getMassSaturn() {
-		return SATURN;
-	}
-	
-	public double getMassUranus() {
-		return URANUS;
-	}
-	
-	public double getMassNeptune() {
-		return NEPTUNE;
-	}
-	
-
+	public static final double MERCURY = 3.18 * Math.pow(10,23);
+	public static final double VENUS = 4.88 * Math.pow(10,24);
+	public static final double EARTH = 5.98 * Math.pow(10,24);
+	public static final double MARS = 6.42 * Math.pow(10,23);
+	public static final double JUPITER = 1.90 * Math.pow(10,27);
+	public static final double SATURN = 5.68 * Math.pow(10,25);
+	public static final double URANUS = 8.68 * Math.pow(10,25);
+	public static final double NEPTUNE = 1.03 * Math.pow(10,26);
 }
