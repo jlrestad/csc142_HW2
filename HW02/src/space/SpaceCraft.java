02eac790c1f419f08fa2357ffe4f230c134c8c33
jlrestad <31015266+jlrestad@users.noreply.github.com>
2018-02-27 22:21:23 -0800

    Updated showData, showDiagnostics

--- HW02/src/space/SpaceCraft.java
+++ HW02/src/space/SpaceCraft.java
@@ -2,10 +2,10 @@
  * Program Name:		SpaceCraft.java
  * Date Created:		February 24th, 2018
  * Author(s): 			Jes Restad
- * Title: 				HW02
+ * Title: 			HW02
  * Description: 		Creates SpaceCraft class and adds data - print commander, 
- * 						pilot, mission specialist, and planetary data
- * Last Updated: 		February 24th, 2018 
+ * 				pilot, mission specialist, and planetary data
+ * Last Updated: 		February 27th, 2018 
  * ********************************************************************************* */
 package space;
 import java.text.DecimalFormat;
@@ -37,7 +37,7 @@ public class SpaceCraft {
 	public String showCommanderData(Person p1) {
 //		return "COMMANDER" + "\nName: " + p1.getName() + "\nAge: " + df.format(commander.getPlanetaryAge()) + " yrs" +
 //				"\nWeight: " + df.format(commander.getPlanetaryWeight()) + " lbs";
-		return commander.getAstronautData();
+		return commander.getAstronautData(); 
 	}
 	
 	public String showPilotData(Person p2) {
@@ -54,14 +54,14 @@ public class SpaceCraft {
 
 	public String showPlanetData() {
 		return "Planet name: " + planet.getName() + "\nDistance from the sun: " + df.format(planet.getDistanceFromSun()) +
-				" m" + "\nTravel around the sun: " + df.format(planet.getNoOfEarthDays()) + " Earth days" + 
-				"\nPlanet mass: " + planet.getMass() + " kg" + "\nPlanet radius: " + df.format(planet.getRadius()) + " m";
+			" m" + "\nTravel around the sun: " + df.format(planet.getNoOfEarthDays()) + " Earth days" + 
+			"\nPlanet mass: " + planet.getMass() + " kg" + "\nPlanet radius: " + df.format(planet.getRadius()) + " m";
 	}
 	
 	//Team discuss efficient & logical way to best implement this method - discuss in report
-	public String showDiagnostics(Person p1, Person p2, Person p3) {
+	public String showDiagnostics(Person p1, Person p2, Person p3) { 
 		return showCommanderData(p1) + "\n\n" + showPilotData(p2)
-				+ "\n\n" + showSpecialistData(p3) + "\n\n" + showPlanetData();
+			+ "\n\n" + showSpecialistData(p3) + "\n\n" + showPlanetData();
 	}
 	
 	
