c3ac9b9df7adc0964e82b4edecfd8c8de9e360ae
jlrestad <31015266+jlrestad@users.noreply.github.com>
2018-02-27 21:39:38 -0800

    Added static

--- HW02/src/space/SunDistance.java
+++ HW02/src/space/SunDistance.java
@@ -2,55 +2,20 @@
  * Program Name:		SunDistance.java
  * Date Created:		February 16th, 2018
  * Author(s): 			Jes Restad
- * Title: 				HW02
+ * Title: 			HW02
  * Description: 		Lists constants for the planet distances from the sun.
- * Last Updated: 		February 18th, 2018 
+ * Last Updated: 		February 27th, 2018 
  * ********************************************************************************* */
 package space;
 
 //planet distances from the sun in m
 public class SunDistance {  
-	public final double MERCURY = 5.79*Math.pow(10, 10);
-	public final double VENUS = 1.08*Math.pow(10, 11);
-	public final double EARTH = 1.50*Math.pow(10, 11);
-	public final double MARS = 2.28*Math.pow(10, 11);
-	public final double JUPITER = 7.78*Math.pow(10, 11);
-	public final double SATURN = 1.43*Math.pow(10, 12);
-	public final double URANUS = 2.87*Math.pow(10, 12);
-	public final double NEPTUNE = 4.50*Math.pow(10,12);
-	
-	public double getDistanceMercury() {
-		return MERCURY;
-	}
-	
-	public double getDistanceVenus() {
-		return VENUS;
-	}
-	
-	public double getDistanceEarth() {
-		return EARTH;
-	}
-	
-	public double getDistanceMars() {
-		return MARS;
-	}
-	
-	public double getDistanceJupiter() {
-		return JUPITER;
-	}
-	
-	public double getDistanceSaturn() {
-		return SATURN;
-	}
-	
-	public double getDistanceUranus() {
-		return URANUS;
-	}
-	
-	public double getDistanceNeptune() {
-		return NEPTUNE;
-	}
-		
-
-	
+	public static final double MERCURY = 5.79*Math.pow(10, 10);
+	public static final double VENUS = 1.08*Math.pow(10, 11);
+	public static final double EARTH = 1.50*Math.pow(10, 11);
+	public static final double MARS = 2.28*Math.pow(10, 11);
+	public static final double JUPITER = 7.78*Math.pow(10, 11);
+	public static final double SATURN = 1.43*Math.pow(10, 12);
+	public static final double URANUS = 2.87*Math.pow(10, 12);
+	public static final double NEPTUNE = 4.50*Math.pow(10,12);
 }
