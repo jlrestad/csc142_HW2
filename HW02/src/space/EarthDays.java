d031ac69302e0462cc374fc558f8f8a3f3001c58
jlrestad <31015266+jlrestad@users.noreply.github.com>
2018-02-27 21:34:40 -0800

    Added static

--- HW02/src/space/EarthDays.java
+++ HW02/src/space/EarthDays.java
@@ -4,53 +4,18 @@
  * Author(s): 			Marianne Goldin
  * Title:                       HW02
  * Description: 		Lists constants for approx. number of Earth days for a planet to travel around the sun.
- * Last Updated: 		February 19th, 2018 
+ * Last Updated: 		February 27th, 2018 
  * ********************************************************************************* */
 package space;
 
 //planet trip around sun duration in Earth days
 public class EarthDays {
-	public final double MERCURY =  88.03;
-	public final double VENUS =  224.63;
-	public final double EARTH =  365.25;
-	public final double MARS =  686.67;
-	public final double JUPITER  4346.48;
-	public final double SATURN = 10774.90;
-	public final double URANUS =  30681.00;
-	public final double NEPTUNE = 60266.30;
-	
-		
-	public double getEarthDaysMercury() {
-		return MERCURY;
-	}
-	
-	public double getEarthDaysVenus() {
-		return VENUS;
-	}
-	
-	public double getEarthDaysEarth() {
-		return EARTH;
-	}
-	
-	public double getEarthDaysMars() {
-		return MARS;
-	}
-	
-	public double getEarthDaysJupiter() {
-		return JUPITER;
-	}
-	
-	public double getEarthDaysSaturn() {
-		return SATURN;
-	}
-	
-	public double getEarthDaysUranus() {
-		return URANUS;
-	}
-	
-	public double getEarthDaysNeptune() {
-		return NEPTUNE;
-	}
-	
-
+	public static final double MERCURY =  88.03;
+	public static final double VENUS =  224.63;
+	public static final double EARTH =  365.25;
+	public static final double MARS =  686.67;
+	public static final double JUPITER  4346.48;
+	public static final double SATURN = 10774.90;
+	public static final double URANUS =  30681.00;
+	public static final double NEPTUNE = 60266.30;
 }
