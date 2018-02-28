d690b2f535358c776c3d443708448ab06811e044
jlrestad <31015266+jlrestad@users.noreply.github.com>
2018-02-27 22:03:50 -0800

    Updated formulas and getPlanetData

--- HW02/src/space/Planet.java
+++ HW02/src/space/Planet.java
@@ -9,40 +9,44 @@
 package space;
 import java.text.DecimalFormat;
 
-//define and print out planet
 public class Planet {
-	
-	private double distanceFromSun;//value in m
+	private double distanceFromSun;//m
 	private double planetEarthDays;
 	private String name;
-	private double mass;//value in kg
-	private double radius;//value in m
+	private double mass; //kg
+	private double radius; //m
 	
-	//value constants
-	private final double G = 6.67 * Math.pow(10,-11); //gravitational constant
-	private final double LIGHT_SPEED = 2.99792458 * Math.pow(10,8); // speed of light in m/s
-	private final double PI = 3.14159265359; // PI, accurate to the 11th decimal pt
-	private final double TO_MILES = 1609.344; //conversion constant from meters to miles, using division
-	private final double TO_MINUTES = 60.0; //conversion constant from sec to min, using division
-	private final double TO_DAYS = 24.0; //conversion constant from hrs to days, using division
+	private final double G = 6.67*Math.pow(10, -11); // gravitational constant
+	private final double LIGHT_SPEED = 2.99792458*Math.pow(10, 8); // m/s
+	private final double PI = 3.14159265359;
+	private final double TO_MILES = 1609.344; // (m/1609.344)
+	private final double TO_MINUTES = 60.0; // (seconds/60.0)
+	private final double TO_DAYS = 24.0; // (hours/24.0)
 	
-	//Planet constructor
-	public Planet (double d, double pED, 
-			String n, double m, double r) {
-		this.distanceFromSun = d;
-		this.planetEarthDays = pED;
+	//main constructor 
+	public Planet() {
+		this.name = null;
+		this.distanceFromSun = 0;
+		this.planetEarthDays = 0;
+		this.mass = 0;
+		this.radius = 0;
+	}
+	
+	//overloaded constructor
+	public Planet(String n, double s, double e, double m, double r) {
 		this.name = n;
+		this.distanceFromSun = s;
+		this.planetEarthDays = e;
 		this.mass = m;
 		this.radius = r;
 	}
 	
-	//access data
 	public double getDistanceFromSun() {
-		return distanceFromSun / TO_MILES; //converts from meters to miles
+		return distanceFromSun / TO_MILES;  //meters to miles
 	}
 	
 	public double getNoOfEarthDays() {
-		return planetEarthDays; 
+		return planetEarthDays;  
 	}
 	
 	public String getName() {
@@ -50,46 +54,34 @@ public class Planet {
 	}
 	
 	public double getMass() {
-		return mass; //in kg
+		return mass;  //kg
 	}
 	
 	public double getRadius() {
-		return radius / TO_MILES; //converts from meters to miles
+		return radius / TO_MILES;  //mi
 	}
 	
-	//get calculated aspects
-	public double getFreeFall(double m, double r) {
-		return ((G * m) / (r * r));//freefall acceleration (m/s.s)
-		//Q: Is this a correct return value for unit m/s.s?
+	public double getFreeFall() {
+		return ((G * mass) / (radius * radius));// m/s.s
 	}
 	
-	/* 
-	 public double getTimeForSunlight(double dm) {
-	 
-	 	return dm / LIGHT_SPEED; //distance from sun (meters) / light speed (meter per second) 
-	 							//time for sunlight to reach the destination planet in mins
-		
-			Q: Formula is not defined in HW conversions, should we assume this is: ??
-					Distance from sun (in meters) / Light speed (in meters)
-					ref: https://phys.org/news/2013-04-sunlight-earth.html
-	}
-	*/
-	
-	public double getSpeedofOrbit(double r, double tdays) {
-		return (2 * PI * (r / TO_MILES)) / (tdays * TO_DAYS );   //approx orbital speed in miles/hr
+	public double getTimeForSunlight() {
+		return (distanceFromSun/LIGHT_SPEED)/TO_MINUTES; // m / m/s = s / 60 = min
 	}
 	
-	//print planet data, formatted to two decimal places
+	public double getSpeedOfOrbit() {
+		return (((2*PI*distanceFromSun)/TO_MILES)/(planetEarthDays * TO_DAYS));  // ((2*PI*r)/T)  T=earth days to orbit sun
+		// convert (m to mi)/(days to hrs) = mi/hr
+	}
+	
 	public String getPlanetData() {
 		DecimalFormat df = new DecimalFormat("0.00");
-		return "+---------Planet Data---------+\n"+
-				"Name: " + name + "\n"+
-				"Free fall acceleration (m/s.s): " + this.getName() + "\n"+
-				"Mass (kg): " + df.format(this.getMass()) + "\n"+
-				"Radius (miles): " + df.format(this.getRadius()) + "\n"+
-				"Distance from sun (miles): " + df.format(this.getDistanceFromSun()) + "\n"+
-				"Number of days to orbit Sun (Earth days): " + df.format(this.getNoOfEarthDays()) + "\n"+
-				"Time for sunlight (minutes): " + "TBD" + "\n"+
-				"+----------------------------+";
+		return "----------------------" + "\nPlanet name: " + this.name + "\nPlanet Sun Distance: " + 
+			this.distanceFromSun + " mi" + "\nPlanet Earth Days: " + this.planetEarthDays + " days" + 
+			"\nPlanet mass: " + this.mass + " kg" + "\nPlanet Radius: " + this.radius + " mi" + "\nFree fall: " 
+			+ df.format(getFreeFall()) + " m/s.s" + "\nTime for sunlight: " + df.format(getTimeForSunlight()) + 
+			" min" + "\nSpeed of orbit: " + df.format(getSpeedOfOrbit()) + " mi/hr" + "\n------------------------\n";
 	}
+
+	
 }
